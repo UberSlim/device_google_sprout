449710e2ed29e684af271f020019a546c7ef48e7
Akhil Narang <akhilnarang.1999@gmail.com>
2015-07-19 13:16:48 +0530

    slim
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ slim.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/google/sprout/sprout.mk)
+
+PRODUCT_RELEASE_NAME := sprout
+
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Common SlimLP stuff
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
+
+PRODUCT_NAME := slim_sprout
+PRODUCT_DEVICE :=sprout
+PRODUCT_BRAND := google
+PRODUCT_MANUFACTURER := Google
+PRODUCT_MODEL := A1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="sprout"
